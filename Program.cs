167b6cfdc0cd7e91cec25bcfc63e47e7aca1a2a5
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2023-02-14 06:39:03 +0300

    Task 41

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Task 41");
+Console.WriteLine("------");
+
+int[] inputArray = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+
+Console.WriteLine($"Массив: [{string.Join(", ", inputArray)}]");
+
+int ReleaseArray1(int[] array)
+{
+  int count = 0;
+  foreach (int item in array)
+  {
+    if (item > 0) count++;
+  }
+  return count;
+}
+
+Console.WriteLine($"Чисел больше ноля: {ReleaseArray1(inputArray)}");
+
+Console.ReadLine();
+
+/*--------------------------------------------*/
